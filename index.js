27f442453963aed0e61583a38315e721ce0328a5
Innobo <innobo@innobos-air.local>
2020-01-16 18:05:37 +0300

    jfkskfs

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('test server');
+/* const http = require('http');
+
+const server = http.createServer((req,res) =>{
+    if(req.url == '/'){
+        res.write('hello nodejs home');
+        res.end();
+    }
+    if(req.url == '/test'){
+        res.end('hello nodejs test');
+        
+    }
+    
+});
+
+app.get('/')
+
+
+server.listen(3000,()=>{
+    console.log('listerning.......');
+}); */
+
+const express = require('express');
+const app = express();
+
+app.get('/:name', (req,res)=>{
+    res.send('hello world'+ req.params.name);
+});
+
+app.listen(3000,()=>{
+    console.log('fgsgfgdgd ......');
+})
